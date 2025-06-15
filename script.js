1b391c22e5f11c1241910e8e6c9c2e284dc793b1
nz621 <nehakhan422012@gmail.com>
2025-06-15 20:31:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("loginForm").style.display = "flex";
+  }, 5000);
+});
